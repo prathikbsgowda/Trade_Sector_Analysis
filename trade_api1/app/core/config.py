1a471a86925c4de829cbaf7c7d1958525e867945
Prathik B S Gowda <96683588+prathikbsgowda@users.noreply.github.com>
2025-07-16 15:04:59 +0530

    Update config.py

--- trade_api1/app/core/config.py
+++ trade_api1/app/core/config.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
+API_KEY ="AIzaSyBqxWR_hgaUW1mbTQpTsgiGiuAmAUmKS8Q"
 NEWS_API_KEY = "9762bd8961fd477fa7b0a1972b766f12"
 
